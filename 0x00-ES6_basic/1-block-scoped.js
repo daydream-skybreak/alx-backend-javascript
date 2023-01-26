4b6aa9c4bb00187765527642f39962b2cc424a98
Samuel <samueldesta323@gmail.com>
2023-01-27 01:42:41 +0300

    added file 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+    const task = false;
+    const task2 = true;
 
-  if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-    task = task2;
-    task2 = task;
-  }
+    if (trueOrFalse) {
+        let task = true;
+        let task2 = false;
+        task = task;
+        task2 = task2;
+    }
 
-  return [task, task2];
+    return [task, task2];
 }
