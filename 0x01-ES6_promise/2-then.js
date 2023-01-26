6596009fb04579e2f9f305fd04ab3e7084bd59b9
Samuel <samueldesta323@gmail.com>
2023-01-26 03:59:54 +0300

    fixed the linter:

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
-export default function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
-    .then((response) => {
-      return {
-        status: 200,
-        body: 'success'
-      };
-    }).catch(() => new Error())
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
