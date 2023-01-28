c197a12e0269e41385a6140f03400f6f1acd406d
Samuel <samueldesta323@gmail.com>
2023-01-28 05:14:29 +0300

    fixed some bug

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   for (const idx of array) {
     array.push(appendString + idx);
   }
-  return array;
+  return newArray;
 }
