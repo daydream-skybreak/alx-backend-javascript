65f68f113899352beccc8cad85baaf9c0c866e29
Samuel <samueldesta323@gmail.com>
2023-01-31 00:44:41 +0300

    added task 0 with eslint and some modification

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,5 @@
-const returnObject = (i, nme, loctn) => ({
-  id: i,
-  name: nme,
-  location: loctn,
-});
-
 export default function getListStudents() {
-  return [returnObject(1, 'Guillaume', 'San Francisco'),
-    returnObject(2, 'James', 'Columbia'),
-    returnObject(5, 'Serena', 'San Francisco')];
+  return [{ id: 1, name: 'Guillaume', location: 'San Francisco' },
+    { id: 2, name: 'James', location: 'Columbia' },
+    { id: 5, name: 'Serena', location: 'San Francisco' }];
 }
