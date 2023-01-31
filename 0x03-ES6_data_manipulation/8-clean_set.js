1206882507c0e8bb0ee24d35d62523888c27a242
Samuel <samueldesta323@gmail.com>
2023-01-31 20:10:01 +0300

    fixed task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 const cleanSet = (set, start) => {
   const str = [];
   const lasti = start.length - 1;
-  if (start === '') str.push('');
-  else {
-    for (const i of set) {
-      if (i.startsWith(start)) str.push(i.slice(lasti + 1));
-    }
-  }
+  if (start === '' || typeof start !== 'string') return '';
+
+  set.forEach((i) => {
+    if (i.startsWith(start)) str.push(i.slice(lasti + 1));
+  });
+
   return [...new Set(str)].join('-');
 };
 export default cleanSet;
