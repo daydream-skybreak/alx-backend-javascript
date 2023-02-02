66a475d24e42a03e7ab54aaf8c4b5299a52751a0
Samuel <samueldesta323@gmail.com>
2023-02-02 03:49:49 +0300

    modified file to fit task 6 requirements

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,5 +34,14 @@ function createEmployee (salary:(number | string)): (Director | Teacher){
     if (typeof salary === 'number' && salary < 500) return new Teacher();
     return new Director()
 }
+function isDirector(employee: (Director | Teacher)){
+    return employee instanceof Director
+}
 
+function executeWork(employee: (Director | Teacher)) {
+    if(isDirector(employee)) {
+        return (employee as Director).workDirectorTasks()
+    }
+    return (employee as Teacher).workTeacherTasks()
+}
 
