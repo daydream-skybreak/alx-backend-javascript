028ba44a8af356d15b83e4f3bf74449f24cdaab2
Samuel <samueldesta323@gmail.com>
2023-02-02 03:16:14 +0300

    added task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,28 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = function(firstName, lastName) {
     return `${firstName[0]} ${lastName}`;
 }
+
+interface iStudentClassConstructor {
+    new(fName:string, lName:string): iStudentClass
+}
+interface iStudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements iStudentClass {
+    firstName: string
+    lastName: string
+
+    constructor(fName:string, lName:string) {
+        this.firstName = fName
+        this.lastName = lName
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+    displayName(): string {
+        return this.firstName
+    }
+}
