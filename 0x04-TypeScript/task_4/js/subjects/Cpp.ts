84afbb4cb839124ecef9e31f372b0eac69a349fb
Samuel <samueldesta323@gmail.com>
2023-02-02 05:02:47 +0300

    finised task 9

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,16 @@
+namespace Subjects{
+    export interface Teacher {
+        experienceTeachingC?: number
+    }
+
+    export class Cpp extends Subjects.Subject {
+        getrequirements(): string {
+            return 'Here is the list of requirement for Cpp'
+        }
+        getAvailableTeacher():string {
+            if (!this.teacher || this.teacher.experienceTeachingC <=0)
+                return 'No available teacher'
+            return `Available Teacher: ${this.teacher.firstName}`
+        }
+    }
+}
