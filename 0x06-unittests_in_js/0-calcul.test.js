f1efb05aaba756d327b841c2fdc4703e44174412
Samuel <samueldesta323@gmail.com>
2023-03-22 22:59:28 +0300

    added task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,24 +3,15 @@ const calculateNumbers = require('./0-calcul')
 describe('0-calcul', function () {
     describe('#calculateNumber()', function () {
         it('should return the sum of the rounded variables', function () {
-            let val = calculateNumbers(1, 2);
+            let val = calculateNumbers(1, 3);
             assert.deepStrictEqual(typeof(val), 'number')
-
-            assert.deepStrictEqual(val, 3);
-            val = calculateNumbers(1, -2);
-            assert.deepStrictEqual(val, -1);
-            val = calculateNumbers(-1, 2);
-            assert.deepStrictEqual(val, 1);
-            val = calculateNumbers(-1, -2);
-            assert.deepStrictEqual(val, -3)
-            val = calculateNumbers(1.5, 2.4);
             assert.deepStrictEqual(val, 4)
-            val = calculateNumbers(1.5, -2.4);
-            assert.deepStrictEqual(val, 0);
-            val = calculateNumbers(-1.5, 2.4);
-            assert.deepStrictEqual(val, 1);
-            val = calculateNumbers(-1.5, -2.4);
-            assert.deepStrictEqual(val, -3);
+            val = calculateNumbers(1, 3.7)
+            assert.deepStrictEqual(val, 5)
+            val = calculateNumbers(1.2, 3.7)
+            assert.deepStrictEqual(val, 5)
+            val = calculateNumbers(1.5, 3.7)
+            assert.deepStrictEqual(val, 6)
         });
     })
 })
