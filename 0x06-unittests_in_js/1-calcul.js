d5ac56d6490ce908557c9574c0991901948e94ff
Samuel <samueldesta323@gmail.com>
2023-03-22 23:56:24 +0300

    modified task 1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,9 +1,9 @@
 const calculateNumber = (op, a, b) => {
     const operations = {
         'SUM': () => Math.round(a) + Math.round(b),
-        'SUBTRACTION': () => Math.round(a) + Math.round(b),
+        'SUBTRACT': () => Math.round(a) - Math.round(b),
         'DIVIDE': () => {
-            if (b !== 0) return Math.round(a) + Math.round(b)
+            if (Math.round(b) !== 0) return Math.round(a) / Math.round(b)
             else return 'Error'
         },
     }
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -30,3 +30,81 @@ describe('#SUM', () => {
         assert.deepStrictEqual(val, -3)
     });
 })
+/**
+ * subtraction testing
+ */
+describe('#SUBTRACTION', () => {
+    it('should pass for positive rounded subtraction', function () {
+        let val = calculateNumber('SUBTRACT', 1, 3);
+        assert.deepStrictEqual(val, -2)
+        val = calculateNumber('SUBTRACT',1, 3.7)
+        assert.deepStrictEqual(val, -3)
+        val = calculateNumber('SUBTRACT',3.7, 1.2)
+        assert.deepStrictEqual(val, 3)
+        val = calculateNumber('SUBTRACT',3.7, 1.5)
+        assert.deepStrictEqual(val, 2)
+    });
+    it('should pass for negative rounded subtraction', function () {
+
+        let val = calculateNumber('SUBTRACT', -2.5, -3.7)
+        assert.deepStrictEqual(val, 2)
+        val = calculateNumber('SUBTRACT', -3.2, -2.5)
+        assert.deepStrictEqual(val, -1)
+        val = calculateNumber('SUBTRACT', -2, -3)
+        assert.deepStrictEqual(val, 1)
+        val = calculateNumber('SUBTRACT', -2.7, -3.7)
+        assert.deepStrictEqual(val, 1)
+    });
+    it('should pass for combination of -ve and +ve rounded "-"', function () {
+        let val = calculateNumber('SUBTRACT', -1.5, 3.7)
+        assert.deepStrictEqual(val, -5)
+        val = calculateNumber('SUBTRACT', 1.2, -3.7)
+        assert.deepStrictEqual(val, 5)
+    });
+})
+/**
+ * division testing
+ */
+describe('#DIVIDE', () => {
+    it('should pass for positive rounded divide', function () {
+        let val = calculateNumber('DIVIDE', 4, 2);
+        assert.deepStrictEqual(val, 2)
+        val = calculateNumber('DIVIDE',7.7, 2.3)
+        assert.deepStrictEqual(val, 4)
+        val = calculateNumber('DIVIDE',5.5, 3.6)
+        assert.deepStrictEqual(val, 1.5)
+        val = calculateNumber('DIVIDE',1.5, 3.7)
+        assert.deepStrictEqual(val, 0.5)
+    });
+    it('should pass for negative rounded divide', function () {
+
+        let val = calculateNumber('DIVIDE', -2.5, -3.7)
+        assert.deepStrictEqual(val, 0.5)
+        val = calculateNumber('DIVIDE', -3.2, -2.5)
+        assert.deepStrictEqual(val, 1.5)
+        val = calculateNumber('DIVIDE', -2, -5)
+        assert.deepStrictEqual(val, 0.4)
+        val = calculateNumber('DIVIDE', -2.7, -3.7)
+        assert.deepStrictEqual(val, 0.75)
+    });
+    it('should pass for combination of -ve and +ve rounded "/"', function () {
+        let val = calculateNumber('DIVIDE', -1.5, 3.7)
+        assert.deepStrictEqual(val, -0.25)
+        val = calculateNumber('DIVIDE', 1.2, -3.7)
+        assert.deepStrictEqual(val, -0.25)
+        val = calculateNumber('DIVIDE', -18, 3)
+        assert.deepStrictEqual(val, -6)
+    });
+
+    it('should return string message "Error"', function () {
+        let val = calculateNumber('DIVIDE', 13, 0)
+
+        assert.deepStrictEqual(val, "Error")
+        val = calculateNumber('DIVIDE', -13, 0)
+        assert.deepStrictEqual(val, "Error")
+        val = calculateNumber('DIVIDE', 13.5, 0)
+        assert.deepStrictEqual(val, "Error")
+        val = calculateNumber('DIVIDE', -13.5, 0)
+        assert.deepStrictEqual(val, "Error")
+    });
+})
